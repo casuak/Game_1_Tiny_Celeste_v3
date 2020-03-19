d4037d9701b64b74fdfb3e164adc737c0f96d50d
wyj <531442894@qq.com>
2020-03-19 10:48:34 +0800

    PrefabTileMap v1.2 bug修复版，基本可用

--- Assets/Scripts/TinyCeleste/02_Modules/05_PrefabTile/02_Balloon/E_Balloon.cs
+++ Assets/Scripts/TinyCeleste/02_Modules/05_PrefabTile/02_Balloon/E_Balloon.cs
@@ -1,14 +1,12 @@
-using System;
-using TinyCeleste._01_Framework;
-using TinyCeleste._02_Modules._05_Env._01_Map;
 using TinyCeleste._02_Modules._07_Physics._04_ColliderChecker;
+using TinyCeleste._06_Plugins._01_PrefabTileMap;
 
-namespace TinyCeleste._02_Modules._05_Env._02_Balloon
+namespace TinyCeleste._02_Modules._05_PrefabTile._02_Balloon
 {
     // 当玩家的冲刺次数小于最大冲刺次数时
     // 与气球碰撞后可重新恢复到最大冲刺速度，同时气球摧毁
     // 一般情况下，气球播放飘动的动画
-    public class E_Balloon : E_MapElement
+    public class E_Balloon : E_PrefabTile
     {
         public C_Balloon_ResumeDashCount resumePlayerDashCount;
 
